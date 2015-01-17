92890f583d2c0843c7d1e4e2f22224525ea37ddf
Bai-Jie <BaiJie1991@gmail.com>
2015-01-17 09:20:09 +0800

    odds and ends
    remove dependency 'com.android.support:appcompat-v7'
    move MainActivity to gq.baijie.voicebroadcastdevice.ui.activity package
    add values-zh

--- app/src/main/java/gq/baijie/voicebroadcastdevice/ui/activity/MainActivity.java
+++ app/src/main/java/gq/baijie/voicebroadcastdevice/ui/activity/MainActivity.java
@@ -1,12 +1,14 @@
-package gq.baijie.voicebroadcastdevice;
+package gq.baijie.voicebroadcastdevice.ui.activity;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import gq.baijie.voicebroadcastdevice.R;
 
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
